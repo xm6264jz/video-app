15fdbca27725dbaa0d74a2649f8cad5c0d71b749
xm6264jz <ahmed.abdinoor3@gmail.com>
2020-11-21 17:28:14 -0600

    created app

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+
+        # String displayed in the admin console, or when printing a model object. 
+        # You can return any useful string here. Optionally 
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
+
+     
